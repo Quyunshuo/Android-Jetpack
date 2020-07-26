458e60fe6dc2c7fa3e6b6b9be9b601899156aaa6
Quyunshuo <quyunshuo@qq.com>
2020-07-26 16:23:01 +0800

    Navigation: DeepLink 深层链接

--- Navigation/src/main/java/com/quyunshuo/navigation/MainFragment.kt
+++ Navigation/src/main/java/com/quyunshuo/navigation/MainFragment.kt
@@ -1,13 +1,18 @@
 package com.quyunshuo.navigation
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_DEFAULT
+import android.app.PendingIntent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavAction
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import com.quyunshuo.navigation.databinding.ActivityMainBinding
 import com.quyunshuo.navigation.databinding.FragmentMainBinding
 
 /**
@@ -33,18 +38,57 @@ class MainFragment : Fragment() {
     }
 
     private fun initView() {
-
-        val bundle = SecondFragmentArgs.Builder()
-            .setUserName("Quyunshuo")
-            .setAge(22)
-            .build()
-            .toBundle()
-
         mBinding.mToSecondFragmentBtn.setOnClickListener(
             Navigation.createNavigateOnClickListener(
                 R.id.action_mainFragment_to_secondFragment,
-                bundle
+                SecondFragmentArgs.Builder()
+                    .setUserName("Quyunshuo")
+                    .setAge(22)
+                    .build()
+                    .toBundle()
             )
         )
+        mBinding.mSendNotificationBtn.setOnClickListener { sendNotification() }
+    }
+
+    /**
+     * 发送一条通知 通过点击通知进行深层链接跳转行
+     */
+    private fun sendNotification() {
+        activity?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notificationChannel =
+                    NotificationChannel("1", "ChannelName", IMPORTANCE_DEFAULT)
+                notificationChannel.description = "description"
+                val notificationManager = it.getSystemService(NotificationManager::class.java)
+                notificationManager.createNotificationChannel(notificationChannel)
+            }
+            val notificationCompat = NotificationCompat.Builder(it,"1")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("DeepLinkDemo")
+                .setContentText("Hello DeepLink!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(getPendingIntent()) // 设置PendingIntent
+                .setAutoCancel(true)
+                .build()
+            val notificationManagerCompat = NotificationManagerCompat.from(it)
+            notificationManagerCompat.notify(8, notificationCompat)
+        }
+    }
+
+    /**
+     * 通过 PendingIntent 设置当通知被单击时需要跳转的 destination 以及传递的参数
+     */
+    private fun getPendingIntent(): PendingIntent? {
+        return activity?.let {
+            val bundle = Bundle()
+            bundle.putString("params", "测试")
+            Navigation.findNavController(it, R.id.mSendNotificationBtn)
+                .createDeepLink()
+                .setGraph(R.navigation.nav_graph)
+                .setDestination(R.id.secondFragment)
+                .setArguments(bundle)
+                .createPendingIntent()
+        }
     }
 }
